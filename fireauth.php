1fdd7656e13990014c158d2a2d788720051a8b1b
Chatura Dilan Perera <dilan@dilan.me>
2019-11-21 09:30:50 +0530

    releasing 1.0.3

--- fireauth.php
+++ fireauth.php
@@ -3,7 +3,7 @@
  * Plugin Name: FireAuth Plugin
  * Plugin URI: http://www.dilan.me
  * Description: Plugin Firebase Authentication.
- * Version: 1.0
+ * Version: 1.0.3
  * Author: Chatura Dilan
  * Author URI: http://www.dilan.me
  */

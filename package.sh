832bfd8b42358ae2ca8a5e400a8bc33e5c50f96a
Chatura Dilan Perera <dilan@dilan.me>
2019-11-27 06:11:17 +0530

    changes to package.sh

--- package.sh
+++ package.sh
@@ -3,4 +3,4 @@
 composer install --no-dev --optimize-autoloader
 rm -rf dist
 mkdir dist
-zip -r dist/fireauth.zip  .  -x "package.sh" -x "composer.json" -x "composer.lock" -x "*.DS_Store" -x ".idea/*" -x "dist/*"  -x ".git/*"  -x ".gitignore"
+zip -r dist/fireauth.zip  .  -x "README.md"  -x "package.sh" -x "composer.json" -x "composer.lock" -x "*.DS_Store" -x ".idea/*" -x "dist/*"  -x ".git/*"  -x ".gitignore"
